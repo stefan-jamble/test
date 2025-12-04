d015c1f96fc36cfd5f1bf74d173ae27155e9e819
Stefan <stefan@jamble.com>
2025-12-04 19:20:04 +0100

    hello

--- src/app/invite/buyer/[id]/page.tsx
+++ src/app/invite/buyer/[id]/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 import Image from "next/image";
 
-export default function Home() {
+interface PageProps {
+    params: {
+        referrer_id: string;
+    };
+}
+
+export default async function Home({ params }: PageProps) {
+    const { referrer_id } = await params;
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
